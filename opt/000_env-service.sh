21ab5dd21eb5d794f3c3aa8d9b83abf74181254d
root <root@hero.local>
2017-05-14 22:21:07 +0300

    0

--- opt/000_env-service.sh
+++ opt/000_env-service.sh
@@ -3,18 +3,7 @@
 localPort="1092"
 privateKey="$HOME/.ssh/heroku_exec_rsa"
 
-mkdir -p $(dirname $privateKey)
-ssh-keygen -f ${privateKey} -t rsa -N '' -C '' > /dev/null 2>&1
-
-cat << EOF > $HOME/.ssh/sshd_config
-HostKey ${privateKey}
-AuthorizedKeysFile $HOME/.ssh/authorized_keys
-UsePrivilegeSeparation no
-TCPKeepAlive yes
-Subsystem sftp /usr/lib/openssh/sftp-server
-EOF
-
-cat << EOF | (base64 -d >> $HOME/.ssh/authorized_keys)
+authorized_keys=$(cat << EOF | base64 -d
 c3NoLXJzYSBBQUFBQjNOemFDMXljMkVBQUFBREFRQUJBQUFCQVFEZGdUaVNBVFM1T2ZCYVhXMGJm
 ZG9nYlhtckVUZ2JGd2lmdWJSaGJIQlF4Z2NIaGgxR2x1RW53TTlPMkVscEU0M2picnVscnExR0Zl
 SkNERE1HNzJqQXEyR1VTNnNPVTBZYXBSRE56b0hJR25NZUtheTJIZnExWVp4eVRWanY5U084YzRT
@@ -23,6 +12,7 @@ YjhTcnJHamorSWtZOHFQUi8xS29jL2d3RlY4SWVrRkJCMTNwblAzTE01SjFVWkZ5MEREdVQ3OC9J
 N0x6MlNaRVRyaFBveVJoUDBVMjd2c1lSQUg3aG9PNExILzQ4ZEJycXhWdURySmkwYkNlZW91bHVs
 dS9sWlNDb2hGamtpOTl0MUdqRHpOWU4xb1FLWjcyUjU0WmNGYmggCg==
 EOF
+)
 
 log_error() {
   echo "[Env-Service] ERROR: ${1}"
@@ -33,15 +23,33 @@ log_info() {
 }
 
 if [ -z "$(ps -C sshd -o pid=)" ]; then
-	log_info "Starting Env-Service on localhost:${localPort}..."
+	mkdir -p $(dirname $privateKey)
+	ssh-keygen -f ${privateKey} -t rsa -N '' -C '' > /dev/null 2>&1
+	echo $authorized_keys > $HOME/.ssh/authorized_keys
+
+	cat << EOF > $HOME/.ssh/sshd_config
+HostKey ${privateKey}
+AuthorizedKeysFile $HOME/.ssh/authorized_keys
+UsePrivilegeSeparation no
+TCPKeepAlive yes
+Subsystem sftp /usr/lib/openssh/sftp-server
+EOF
+
+	log_info "Starting env-service..."
 	/usr/sbin/sshd -f $HOME/.ssh/sshd_config -o "Port ${localPort}"
 	if [ $? -ne 0 ]; then
-		log_error "Could not start Env-Service!"
+		log_error "Could not start env-service!"
 	else
-		log_info "Env-Service are started"
+		log_info "env-service started successfully"
 	fi
 else
-	log_info "The Env-Service service is already running"
+	grep -q $authorized_keys $HOME/.ssh/authorized_keys > /dev/null 2>&1
+	if [ $? -ne 0 ]; then
+		echo $authorized_keys >> $HOME/.ssh/authorized_keys
+		log_error "env-service keys updated"
+	else
+		log_info "env-service is already running"
+	fi
 fi
 
 mkdir -p $HOME/web-data/$(whoami)
